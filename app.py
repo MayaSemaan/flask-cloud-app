6951820c45b679d73766ee9314eda9c608caf885
unknown <mayasemaan33@gmail.com>
2025-05-21 16:31:37 +0300

    Fix deployment issues

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
 from flask import Flask
-app = Flask(_name_)
+app = Flask(__name__)
 
 @app.route('/')
 def home():
     return '<h1>Hello from Flask on Render!</h1><p>This is a live cloud app.</p>'
 
-if _name_ == '_main_':
+if __name__ == '__main__':
     app.run()
